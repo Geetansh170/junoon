08a37d823fa7c45bf0b997bb15025115ec8b88e9
Geetansh170 <geetanshkumar170@gmail.com>
2020-07-05 16:52:36 +0530

    checking

--- server.js
+++ server.js
@@ -111,6 +111,7 @@ if (process.env.NODE_ENV === 'production') {
   
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      console.log("Backend Working");
     });
   }
 
