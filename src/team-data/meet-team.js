b364e13c3123e0261e945a0c7aff5a3435b396d1
Ishku15 <75108202+Ishku15@users.noreply.github.com>
2021-06-13 03:21:38 +0530

    Meet_the_team-card

---
+++ src/team-data/meet-team.js
@@ -0,0 +1,95 @@
+const team = {
+  members: [
+    {
+      name: "Geetansh Kumar ",
+      year: 3,
+      image: " /memberimage/download.png",
+      description: "Miles to go before I sleep! ",
+      insta: "Geetansh170",
+    },
+    {
+      name: "Divyanshu",
+      year: 2,
+      image: "/memberimage/divyanshu.jpg ",
+      description: " ",
+      insta: "",
+    },
+    {
+      name: "Ishkaran Kochar",
+      year: 2,
+      post: "Front and Back End",
+      image: "/memberimage/ishkaran.jpg",
+      description: "If i can do it so can YOU!",
+      insta: "rather_be_ishku",
+    },
+    {
+      name: "Siddharth Gupta",
+      year: 2,
+      image: "/memberimage/SIDDHARTHGUPTA.jpg",
+      description: " ",
+      insta: "",
+    },
+    {
+      name: "Armaan Chauhan",
+      year: 2,
+      post: "Front End",
+      image: "/memberimage/download.png ",
+      description: "",
+      insta: "designbyarmaan",
+    },
+    {
+      name: "Naman Gogia",
+      year: 2,
+      image: "/memberimage/download.png ",
+      description: " ",
+      insta: "",
+    },
+    {
+      name: "Unnat",
+      year: 2,
+      image: "/memberimage/download.png ",
+      description: " ",
+      insta: "",
+    },
+
+    {
+      name: "Amrutanshu Tyagi",
+      year: 1,
+      image: "/memberimage/download.png",
+      description: "I’m one with the Force and the Force is with me",
+      insta: "amruxiii",
+    },
+    {
+      name: "Adarsh Kumar ",
+      year: 1,
+      image: "/memberimage/download.png ",
+      description: "Consistency is the key.",
+      insta: "adarsh_37",
+    },
+
+    {
+      name: " Devanshi Sharma ",
+      year: 1,
+      image: " /memberimage/download.png",
+      description: " Get up , show up , never give up ",
+      insta: " ",
+    },
+    {
+      name: "Pranav Chauhan",
+      year: 1,
+      post: "Full-Stack",
+      image: "/memberimage/pranav.jpeg",
+      description: "I’m not perfect but I am limited edition.",
+      insta: "thepranav.chauhan",
+    },
+
+    {
+      name: "Jatin Rawat",
+      year: 1,
+      image: "/memberimage/download.png ",
+      description: " ",
+      insta: "",
+    },
+  ],
+};
+export default team;
