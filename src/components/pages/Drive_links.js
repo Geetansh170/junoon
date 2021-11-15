5add08cb44fc326f55d527c0ef6001a9f2e00786
Adarsh Kumar <71789479+envision79@users.noreply.github.com>
2021-11-15 21:46:14 +0530

    Added sports day 2021 card to drive_links.js

--- src/components/pages/Drive_links.js
+++ src/components/pages/Drive_links.js
@@ -84,6 +84,14 @@ const Drive_links = () => {
 </div>
 
 <div className="hovercontainer1">
+  
+  <div className="wrapper">
+  <a href="https://drive.google.com/drive/folders/1LH9-YWNyWglMCTBgQzgUjlHy1QMqt8Ra" className="mainText" target="_blank">
+    <div className="card card6">
+      <p className="headText">Sports Day 2021</p><hr className="headUnderline"></hr><i className="fal"><span className="jnlogo"></span></i></div>
+      </a>
+  </div>
+  
   <div className="wrapper">
   <a href="https://drive.google.com/drive/folders/1dY-dSI54Okm9n6cwopTjRt29bfS4Rd5h?usp=sharing" className="mainText" target="_blank">
     <div className="card card1">
